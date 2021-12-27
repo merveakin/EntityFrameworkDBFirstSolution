53170c64e0a5ccc3b14d90ceb5f891c45c942ec7
merveakin <merveakinnnn@hotmail.com>
2021-12-27 09:38:36 +0300

    Add project files.

---
+++ EntityFrameworkDBFirst_BLL/ProductManager.cs
@@ -0,0 +1,74 @@
+﻿using EntityFrameworkDBFirst_BLL.ViewModels;
+using EntityFrameworkDBFirst_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDBFirst_BLL
+{
+    public class ProductManager
+    {
+        //GLOBAL ALAN 
+        NORTHWNDEntities myDBContext = new NORTHWNDEntities();
+        public List<ProductViewModels> TumUrunleriGetir()
+        {
+            List<ProductViewModels> urunler = new List<ProductViewModels>();
+            var data = myDBContext.Products.ToList();
+
+            foreach (var item in data)
+            {
+                ProductViewModels p = new ProductViewModels()
+                {
+                    CategoryID = item.CategoryID,
+                    ProductID = item.ProductID,
+                    UnitPrice = item.UnitPrice,
+                    UnitsInStock = item.UnitsInStock,
+                    Discontinued = item.Discontinued,
+                    UnitsOnOrder = item.UnitsOnOrder,
+                    ReorderLevel = item.ReorderLevel,
+                    SupplierID = item.SupplierID,
+                    ProductName = item.ProductName,
+                    QuantityPerUnit = item.QuantityPerUnit
+                };
+
+                //CategoryName
+                //1.YOL
+              //  p.CategoryName = item.Categories.CategoryName;
+
+
+                //2.YOL
+                p.CategoryName = myDBContext.Categories.FirstOrDefault(x => x.CategoryID == item.CategoryID)?.CategoryName;
+
+                urunler.Add(p);
+            }
+
+            return urunler;
+        }
+
+
+        //deneme button1 oluşturuldu.
+
+        //public bool YeniUrunEkle(ProductViewModels p)
+        //{
+        //    bool sonuc = false;
+        //    Products newProduct = new Products
+        //    {
+        //        ProductName = p.ProductName,
+        //        Discontinued = p.Discontinued,
+        //        CategoryID = p.CategoryID
+        //    };
+
+        //    myDBContext.Products.Add(newProduct);
+        //    int affectedRows = myDBContext.SaveChanges();
+        //    if (affectedRows > 0)
+        //    {
+        //        sonuc = true;
+        //    }
+
+        //    return sonuc;
+        //}
+
+    }
+}
---
+++ EntityFrameworkDBFirst_WinFormUI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using EntityFrameworkDBFirst_BLL;
+using EntityFrameworkDBFirst_BLL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDBFirst_WinFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //GLOBAL ALAN
+        ProductManager myProductManager = new ProductManager();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Form yüklenirken Grid'e product bilgileri gelsin.
+            GrideProductlariGetir();
+        }
+
+        private void GrideProductlariGetir()
+        {
+            //BLL aracılığıyla listemiz gelecek.
+            dataGridView1.DataSource = myProductManager.TumUrunleriGetir();
+            dataGridView1.Columns["SupplierID"].Visible = false;
+            dataGridView1.Columns["CategoryID"].Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DENEMEK İÇİN YAZIYORUZ.
+            //ProductViewModels yeniUrun = new ProductViewModels() 
+            //{
+            //    ProductName = "deneme ürünü",
+            //    Discontinued = false,
+            //    CategoryID = 1
+            //};
+            // myProductManager.YeniUrunEkle(yeniUrun);
+            //View Model ile aktarım yapmak işi uzatıyor.
+        }
+    }
+}
